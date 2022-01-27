2a239feed2d544e5fd5264cd6f70fcc96d0106cb
barsukzhigul <pain.kenin@gmail.com>
2022-01-27 08:01:01 +0300

    only 1 button left

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+global.$ = {
+  gulp: require('gulp'),
+  gp:   require('gulp-load-plugins')(),
+  browserSync: require('browser-sync').create(),
+  rollup: require('rollup'),
+  imageminJpegRecompress: require('imagemin-jpeg-recompress'),
+  pngquant: require('imagemin-pngquant'),
+  del: require('del'),
+  path: {
+    config: require('./gulp/config.js'),
+    jquery: './js/jquery.js',
+    scrolloverflow: './js/scrolloverflow.min.js',
+    fullpage: './js/fullpage.min.js',
+    js: './js/**/*.js',
+  }
+};
+
+$.path.config.forEach(function(path) {
+  require(path)();
+});
